ea2e0d2e2fab7880ac3975bf33aa8657305f0c77
hujian <swjtu_hj@163.com>
2018-09-28 02:08:00 +0800

    Manacher算法
    O(n)

--- LongestPalindromicSubstring.py
+++ LongestPalindromicSubstring.py
@@ -1,48 +1,42 @@
 # coding: utf-8
-public static String longestPalindrome(String string) {
+# hihocoder第一周: http://hihocoder.com/contest/hiho1/problem/1
+#1. O(n^3) 枚举所有字串
+#2. O(n^2) 枚举字串中心,再枚举字串长度
+#3. O(n) Manacher算法
+#arr = "abacbacbacbcab"
+def GO():
+    arr = raw_input()
+    length = [0]*(len(arr)*2+1)
+    new_arr = "#"
+    longest_p_center = 0
+    longest_p_right = 0
+    start = 0
+    maxlen = 0
+    for i in xrange(len(arr)):
+        new_arr += arr[i] + "#"
+    for i in xrange(len(new_arr)):
+        j = longest_p_center - (i - longest_p_center)
+        if i <= longest_p_right:
+            if i + length[j] < longest_p_right:
+                length[i] = min(length[j], longest_p_right-i)
+            else:
+                length[i] = 1 
+                while i + length[i] < len(new_arr) and i - length[i] > 0 and new_arr[i+length[i]] == new_arr[i-length[i]]:
+                    length[i] += 1
+            if i + length[i] > longest_p_right:
+                longest_p_right = i + length[i]
+                lengest_p_center = i
+            if length[i] > maxlen:
+                start = i
+                maxlen = length[i]
+    ans = ""
+    for i in xrange(len(new_arr)):
+        if i >= start - maxlen + 1 and i <= start+maxlen:
+            ans += new_arr[i]
+    ans = ans.replace('#', '')
+    print len(ans) 
 
-        //-----------------------------------
-        //转换字符串
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("#");
-        for (int i = 0; i < string.length(); i++) {
-            stringBuilder.append(string.charAt(i));
-            stringBuilder.append("#");
-        }
-        //-----------------------------------
-        int rightIndex = 0;
-        int centerIndex = 0;
-        //求len中的最大
-        int answer = 0;
-        //answer最大时的中心
-        int index = 0;
-        int len[] = new int[stringBuilder.length() ];
-        for (int i = 1; i < stringBuilder.length(); i++) {
-            //当rightIndex > i，那么我们就在rightIndex - i 与len[2 * centerIndex - i](len[j])，取得最小值
-            //因为当i + len[j] < rightIndex时，我们就把len[i]更新为len[j]
-            //但是如果i + len[j] >= rightIndex时，我们暂且将len[i]定更新为rightIndex - i,超出的部分需要我们一个一个的匹配
-            if (rightIndex > i) {
-                len[i] = Math.min(rightIndex - i, len[2 * centerIndex - i]);
-            } else {
-                len[i] = 1;
-            }
-            //一个一个匹配
-            //要么是超出的部分，要么是i > rightIndex
-            while(i - len[i] >= 0 && i + len[i] < stringBuilder.length() && stringBuilder.charAt(i - len[i]) == stringBuilder.charAt(i + len[i])) {
-                len[i]++;
-            }
-            //当 len[i] + i > rightIndex,我们需要更新centerIndex和rightIndex
-            //至于为什么会这样做，理解一下rightIndex和centerIndex的含义
-            if(len[i] + i > rightIndex) {
-                rightIndex = len[i] + i;
-                centerIndex = i;
-            }
-            if(len[i] > answer) {
-                answer = len[i];
-                index = i;
-            }
-        }
-        //截取字符串
-        //为什么index - answer + 1,因为len[i] - 1才是原来的回文字符串长度，而answer记录的是len中最大值
-        return stringBuilder.substring(index - answer + 1, index + answer).replace("#", "");
-    }
+if __name__ == '__main__':
+    for i in xrange(input()):
+        GO()
+#难道是因为后来才添加的python。。之前的题目就没跑时间？
